ffe83e0a6f86599d72e5a9b7888285c00f5e9c74
23f1003075 <23f1003075@ds.study.iitm.ac.in>
2026-07-12 12:58:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+
+EMAIL = "23f1003075@ds.study.iitm.ac.in"
+AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIzZjEwMDMwNzVAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4MjYyNzkyOSwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4MzIzMjcyOX0.29A4L6lN2B4tkmvMogXNnE-LMQFeBGgyStrazu2xvEA"
+AIPIPE_BASE = "https://aipipe.org/openai/v1"
+TEXT_MODEL = "gpt-4o-mini"
+VISION_MODEL = "gpt-4o"          
+EMBED_MODEL = "text-embedding-3-small"
